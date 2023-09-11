8c24749dae0716aa66e38c0b7808d6622848c392
Alexandru G <alexandrugugurel@gmail.com>
2023-09-11 19:06:00 +0300

    Setup for create GovNFT

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import DarkThemeToggle from "./DarkThemeToggle";
 
 export default function Footer() {
   return (
-    <div className="flex justify-between items-center text-xs border-t border-black/5 dark:border-white/5 py-4 mt-24">
+    <div className="flex justify-between items-center text-xs border-t border-black/5 dark:border-white/5 pt-4 pb-24 mt-24">
       {/* @ts-ignore */}
       <span className="opacity-40">2023 &copy; GOVNFT. {__APP_VERSION__}</span>
-      <div className="flex gap-8 items-center">
+      <div className="flex gap-4 items-center">
         <div className="hidden md:flex gap-2 items-center">
           <span className="opacity-40">A public good for</span>{" "}
           <img src="./svg/op-logo.svg" className="h-5" />
