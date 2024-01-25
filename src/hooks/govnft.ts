329a40d5cce90c2193e48dcb1be61afbe6482982
ethzoomer <ethzoomer@gmail.com>
2024-01-25 11:53:15 -0600

    Add govnft hooks

---
+++ src/hooks/govnft.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { readContract } from "@wagmi/core";
+import config from "../rpc";
+import { GovNft } from "./types";
+
+import { GOVNFT_SUGAR_ABI, GOVNFT_SUGAR_ADDRESS } from "../constants";
+
+async function fetchGovNfts(account): Promise<GovNft[]> {
+  const govnfts = await readContract(config, {
+    address: GOVNFT_SUGAR_ADDRESS,
+    abi: GOVNFT_SUGAR_ABI,
+    functionName: "all",
+    args: [account],
+  });
+
+  return govnfts;
+}
+
+export function useGovNfts(accountAddress, opts = {}) {
+  return useQuery({
+    queryKey: ["fetchGovNfts"],
+    queryFn: () => fetchGovNfts(accountAddress),
+    ...opts,
+    placeholderData: [],
+    // @ts-ignore
+    cacheTime: 5_000 * 60, // 5 minute
+    keepPreviousData: true,
+  });
+}
--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -9,3 +9,21 @@ export type Token = {
   symbol: string;
   value: bigint;
 };
+
+export type GovNft = {
+  id: number;
+  total_locked: bigint;
+  amount: bigint;
+  total_claimed: bigint;
+  claimable: bigint;
+  split_count: number;
+  cliff_length: number;
+  start: number;
+  end: number;
+  token: Address;
+  vault: Address;
+  minter: Address;
+  owner: Address;
+  address: Address;
+  delegated: Address;
+};
