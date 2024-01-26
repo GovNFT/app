b4f038d8b04b7dcc3053aee6688032f677a265ce
Alexandru G <alexandrugugurel@gmail.com>
2024-01-26 11:33:27 +0200

    Add Chart Preview

---
+++ src/pages/Create/components/Chart.tsx
@@ -0,0 +1,149 @@
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  LinearScale,
+  LineElement,
+  PointElement,
+} from "chart.js";
+import dayjs, { Dayjs } from "dayjs";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement);
+
+export default function Chart({
+  vestingDuration,
+  vestingInterval,
+  cliffDuration,
+  cliffInterval,
+  startDate,
+}: {
+  vestingDuration: number;
+  vestingInterval: string;
+  cliffDuration: number;
+  cliffInterval: string;
+  startDate: Dayjs | null;
+}) {
+  // @ts-ignore
+  const endDate = dayjs(startDate).add(vestingDuration, vestingInterval);
+  // @ts-ignore
+  const cliffDate = dayjs(startDate).add(cliffDuration, cliffInterval);
+
+  console.log(typeof startDate);
+
+  const options = {
+    // responsive: true,
+    // @ts-ignore
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          color: "rgba(255, 255, 255, 0.5)",
+          padding: 10,
+        },
+      },
+      y: {
+        position: "right",
+        beginAtZero: true,
+        ticks: {
+          display: true,
+          font: {
+            size: 10,
+          },
+          callback: function (value) {
+            return " -  " + value + "%";
+          },
+        },
+      },
+    },
+  };
+
+  const totalWeeks = endDate.diff(startDate, "weeks");
+  const clifWeeks = cliffDate.diff(startDate, "weeks");
+  const cliff = (clifWeeks / totalWeeks) * 100;
+
+  const labels = [
+    startDate.format("MMM YYYY"),
+    "•",
+    "•",
+    "•",
+    "•",
+    endDate.format("MMM YYYY"),
+  ];
+
+  const vestingData =
+    // Display 6 nodes on the graph.
+    // If Cliff is set, we will hide the graph based on the Cliff percentage.
+
+    // No cliff
+    (cliff == 0 && [0, 20, 40, 60, 80, 100]) ||
+    // Cliff 0 to 20 percent
+    (cliff > 0 && cliff <= 20 && [NaN, 20, 40, 60, 80, 100]) ||
+    // Cliff 20 to 40 percent
+    (cliff > 20 && cliff <= 40 && [NaN, NaN, 40, 60, 80, 100]) ||
+    // Cliff 40 to 60 percent
+    (cliff > 40 && cliff <= 60 && [NaN, NaN, NaN, 60, 80, 100]) ||
+    // Cliff higher than 60 percent
+    (cliff > 60 && [NaN, NaN, NaN, NaN, 80, 100]);
+
+  const cliffData =
+    // Calculat ethe length of the cliff.
+
+    // Cliff 0 to 20 percent
+    cliff > 0 && cliff <= 20
+      ? [0, 100]
+      : NaN ||
+        // Cliff 20 to 40 percent
+        (cliff > 20 && cliff <= 40)
+      ? [0, 0, 100]
+      : NaN ||
+        // Cliff 40 to 60 percent
+        (cliff > 40 && cliff <= 60)
+      ? [0, 0, 0, 100]
+      : NaN ||
+        // Cliff 60 to 100 percent
+        (cliff > 60 && cliff <= 100)
+      ? [0, 0, 0, 0, 100]
+      : NaN;
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Vesting",
+        data: vestingData,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235)",
+        borderWidth: 1,
+        radius: 2,
+        fill: "start",
+      },
+      {
+        label: "Cliff",
+        data: cliffData,
+        borderDash: [1, 3],
+        borderColor: "rgb(250, 218, 94)",
+        backgroundColor: "rgba(250, 218, 94)",
+        borderWidth: 1,
+        radius: 2,
+        stepped: true,
+      },
+    ],
+  };
+
+  if (dayjs(cliffDate).isAfter(dayjs(endDate))) {
+    return (
+      <div className="my-8 text-xs bg-black/5 dark:bg-black/5 p-6 text-center">
+        Cliff can't be higher then the vesting period.
+      </div>
+    );
+  }
+
+  return (
+    <div className="my-8 text-xs">
+      {/* @ts-ignore */}
+      <Line options={options} data={data} />
+    </div>
+  );
+}
--- src/pages/Create/components/Creator.tsx
+++ src/pages/Create/components/Creator.tsx
@@ -1,4 +1,11 @@
-import { Datepicker, Select, TextInput, Textarea, ToggleSwitch } from "flowbite-react";
+import dayjs from "dayjs";
+import {
+  Datepicker,
+  Select,
+  Textarea,
+  TextInput,
+  ToggleSwitch,
+} from "flowbite-react";
 import { useEffect, useState } from "react";
 import { isAddress, parseUnits } from "viem";
 import { useAccount } from "wagmi";
@@ -6,8 +13,8 @@ import { useAccount } from "wagmi";
 import AssetInput from "../../../components/AssetInput";
 import { useTokens } from "../../../hooks/token";
 import { Token } from "../../../hooks/types";
+import Chart from "./Chart";
 import Checklist from "./Checklist";
-import Graph from "./Graph";
 import Preview from "./Preview";
 
 export default function Creator() {
@@ -15,12 +22,18 @@ export default function Creator() {
   const [amount, setAmount] = useState(parseUnits("0", 18));
   const [preview, setPreview] = useState(false);
   const [toAddress, setToAddress] = useState(null);
-  const [vestingDuration, setVestingDuration] = useState("1");
-  const [cliffDuration, setCliffDuration] = useState("0");
+
+  const today = dayjs();
+  const [vestingDuration, setVestingDuration] = useState(1);
+  const [cliffDuration, setCliffDuration] = useState(0);
+  const [selectedStartDate, setSelectedStartDate] = useState(today);
+  const [vestingInterval, setVestingInterval] = useState("years");
+  const [cliffInterval, setCliffInterval] = useState("months");
 
   const [recipientName, setRecipientName] = useState("");
   const [tags, setTags] = useState(null);
   const [desc, setDesc] = useState("");
+  const timeframe = ["years", "months", "weeks"];
 
   const { address: accountAddress } = useAccount();
 
@@ -31,11 +44,11 @@ export default function Creator() {
   useEffect(() => {
     // @ts-ignore
     setToken(tokens[0]);
-  }, [tokens, tokens[0]]);
+  }, [tokens]);
 
   useEffect(() => {
     // @ts-ignore
-    if (isAddress(toAddress) && amount !== 0) {
+    if (isAddress(toAddress) && amount != 0) {
       setPreview(true);
     } else {
       setPreview(false);
@@ -47,8 +60,14 @@ export default function Creator() {
       <div className="lg:w-8/12 mb-4 lg:mb-0 bg-white shadow-md dark:bg-white/5 p-2 md:p-6 rounded-lg">
         <div className="px-4 mb-8 mt-4">
           <div className="space-y-3 pb-6">
-            <div className="text-xs text-gray-600 dark:text-gray-400">Recipient Address</div>
-            <TextInput placeholder="0x" value={toAddress} onChange={(e) => setToAddress(e.target.value)} />
+            <div className="text-xs text-gray-600 dark:text-gray-400">
+              Recipient Address
+            </div>
+            <TextInput
+              placeholder="0x"
+              value={toAddress}
+              onChange={(e) => setToAddress(e.target.value)}
+            />
           </div>
 
           <div className="space-y-3 pb-6">
@@ -64,31 +83,74 @@ export default function Creator() {
           </div>
 
           <div className="space-y-3 pb-6">
-            <div className="text-xs text-gray-600 dark:text-gray-400">Start Date</div>
-            <Datepicker minDate={new Date("2023-09-17T22:00:00.000Z")} />
+            <div className="text-xs text-gray-600 dark:text-gray-400">
+              Start Date
+            </div>
+            <Datepicker
+              // @ts-ignore
+              onSelect={(e) => setSelectedStartDate(dayjs(e.target.value))} // not working
+            />
+            {/*<TextInput
+              onSelect={(e) => setSelectedStartDate(e.target.value)} // not working
+              type="date"
+            />*/}
           </div>
 
           <div className="md:flex gap-6">
             <div className="space-y-3 pb-6 grow">
-              <div className="text-xs text-gray-600 dark:text-gray-400">Vesting Duration</div>
+              <div className="text-xs text-gray-600 dark:text-gray-400">
+                Vesting Duration
+              </div>
               <div className="relative">
-                <TextInput value={vestingDuration} onChange={(e) => setVestingDuration(e.target.value)} type="number" />
-                <Select sizing="sm" className="absolute top-0.5 right-0.5 sm:top-1.5 sm:right-1.5 w-24" color="gray">
-                  <option>Years</option>
-                  <option>Months</option>
-                  <option>Weeks</option>
+                <TextInput
+                  value={Number(vestingDuration)}
+                  // @ts-ignore
+                  onChange={(e) => setVestingDuration(e.target.value)}
+                  type="number"
+                  min="0"
+                  step="1"
+                />
+                <Select
+                  sizing="sm"
+                  className="absolute top-0.5 right-0.5 sm:top-1.5 sm:right-1.5 w-24"
+                  color="gray"
+                  onChange={(e) => setVestingInterval(e.target.value)}
+                  defaultValue={vestingInterval}
+                >
+                  {timeframe.map((time, index) => (
+                    <option value={time} key={index}>
+                      {time}
+                    </option>
+                  ))}
                 </Select>
               </div>
             </div>
 
             <div className="space-y-3 pb-6 grow">
-              <div className="text-xs text-gray-600 dark:text-gray-400">Cliff Duration</div>
+              <div className="text-xs text-gray-600 dark:text-gray-400">
+                Cliff Duration
+              </div>
               <div className="relative">
-                <TextInput value={cliffDuration} onChange={(e) => setCliffDuration(e.target.value)} type="number" />
-                <Select sizing="sm" className="absolute top-0.5 right-0.5 sm:top-1.5 sm:right-1.5 w-24" color="gray">
-                  <option>Years</option>
-                  <option>Months</option>
-                  <option>Weeks</option>
+                <TextInput
+                  value={Number(cliffDuration)}
+                  // @ts-ignore
+                  onChange={(e) => setCliffDuration(e.target.value)}
+                  type="number"
+                  min="0"
+                  step="1"
+                />
+                <Select
+                  sizing="sm"
+                  className="absolute top-0.5 right-0.5 sm:top-1.5 sm:right-1.5 w-24"
+                  color="gray"
+                  onChange={(e) => setCliffInterval(e.target.value)}
+                  defaultValue={cliffInterval}
+                >
+                  {timeframe.map((time, index) => (
+                    <option value={time} key={index}>
+                      {time}
+                    </option>
+                  ))}
                 </Select>
               </div>
             </div>
@@ -96,7 +158,9 @@ export default function Creator() {
 
           <div className="space-y-3 pt-3 pb-2 grow">
             <div className="bg-black/[.03] dark:bg-white/[.02] rounded-lg flex items-center px-3.5 py-3">
-              <div className="text-xs text-gray-600 dark:text-gray-400 grow">Transferable</div>
+              <div className="text-xs text-gray-600 dark:text-gray-400 grow">
+                Transferable
+              </div>
               <ToggleSwitch
                 // @ts-ignore
                 color="green"
@@ -113,7 +177,9 @@ export default function Creator() {
             <span className="opacity-50">Optional Details</span>
           </div>
           <div className="space-y-3 pb-6">
-            <div className="text-xs text-gray-600 dark:text-gray-400">Recipient Name</div>
+            <div className="text-xs text-gray-600 dark:text-gray-400">
+              Recipient Name
+            </div>
             <TextInput
               placeholder="e.g. Velodrome Foundation"
               value={recipientName}
@@ -131,17 +197,36 @@ export default function Creator() {
           </div>
 
           <div className="space-y-3 pb-6">
-            <div className="text-xs text-gray-600 dark:text-gray-400">Description</div>
+            <div className="text-xs text-gray-600 dark:text-gray-400">
+              Description
+            </div>
             <Textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
           </div>
         </div>
       </div>
 
       <div className="lg:w-6/12 p-6 sm:p-10 bg-white/90 dark:bg-white/[.08] dark:bg-opacity-50 rounded-lg">
-        <Checklist toAddress={toAddress} amount={amount} vestingDuration={vestingDuration} />
-        <Graph />
+        <Checklist
+          toAddress={toAddress}
+          amount={amount}
+          vestingDuration={vestingDuration}
+        />
+        <Chart
+          startDate={selectedStartDate}
+          vestingDuration={vestingDuration}
+          vestingInterval={vestingInterval}
+          cliffDuration={cliffDuration}
+          cliffInterval={cliffInterval}
+        />
+
         {preview && (
-          <Preview toAddress={toAddress} amount={amount} token={token} recipient={recipientName} desc={desc} />
+          <Preview
+            toAddress={toAddress}
+            amount={amount}
+            token={token}
+            recipient={recipientName}
+            desc={desc}
+          />
         )}
       </div>
     </>
