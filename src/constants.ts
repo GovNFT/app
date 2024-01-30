b3d3a6d8ab3d04c8472299193d295b0af67b1687
Zoomer <87513793+ethzoomer@users.noreply.github.com>
2024-01-30 12:10:15 -0600

    Add govnft hooks (#15)
    * Add govnft hooks
    
    * linter
    
    * Generate wagmi ABIs, fix govnft hook types
    
    * Ignore ABI formatting
    
    * refactor: simplify GovNft types.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,14 @@
 import { optimism } from "viem/chains";
 
+import { govnftSugarAbi } from "./generated";
 import { Address } from "./hooks/types";
 
 export const ZERO_ADDRESS: Address = "0x0000000000000000000000000000000000000000";
 export const TOKEN_ICON = "/svg/coin.svg";
 export const RPC_URI = import.meta.env.VITE_RPC_URI;
+export const GOVNFT_ADDRESS = import.meta.env.VITE_GOVNFT_ADDRESS;
+export const GOVNFT_SUGAR_ADDRESS = import.meta.env.VITE_GOVNFT_SUGAR_ADDRESS;
+export const GOVNFT_SUGAR_ABI = govnftSugarAbi;
 export const TOKEN_ADDRESSES: Address[] = import.meta.env.VITE_TOKEN_ADDRESSES.split(",");
 export const TOKEN_ASSETS_CDN = String(import.meta.env.VITE_TOKEN_ASSETS_CDN).split(",");
 export const DEFAULT_CHAIN = optimism;
--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -9,3 +9,21 @@ export type Token = {
   symbol: string;
   value: bigint;
 };
+
+export type GovNft = {
+  id: bigint;
+  total_locked: bigint;
+  amount: bigint;
+  total_claimed: bigint;
+  claimable: bigint;
+  split_count: bigint;
+  cliff_length: bigint;
+  start: bigint;
+  end: bigint;
+  token: Address;
+  vault: Address;
+  minter: Address;
+  owner: Address;
+  address: Address;
+  delegated: Address;
+};
