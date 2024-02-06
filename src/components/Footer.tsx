7605194c393639330e2b9e5b86b934855fb47f16
Alexandru Gugurel <alexandrugugurel@gmail.com>
2024-02-06 15:39:17 +0200

    Add small UI updates (#22)
    * Add small UI updates
    
    * Update footer date
    
    * Delete src/generated.ts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,9 @@ import DarkThemeToggle from "./DarkThemeToggle";
 export default function Footer() {
   return (
     <div className="flex justify-between items-center text-xs border-t border-black/5 dark:border-white/5 py-4 mt-24 mb-24">
-      {/* @ts-ignore */}
       <span className="text-gray-600 dark:text-gray-400">
         {/* @ts-ignore */}
-        2023 &copy; GOVNFT. {__APP_VERSION__}
+        {new Date().getFullYear()} &copy; GovNFT. {__APP_VERSION__}
       </span>
       <div className="flex gap-8 items-center">
         <div className="hidden md:flex gap-2 items-center">
