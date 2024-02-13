56b293f3bdae127f3831a7a85540a620c236fa53
Alexandru Gugurel <alexandrugugurel@gmail.com>
2024-02-13 11:05:39 +0200

    Update delegation page (#29)
    * Add govnft navigation
    
    * Update gitignore
    
    * Run linter
    
    * Remove useSearchParams
    
    * Remove activity
    
    * Run linter
    
    * Run linter
    
    * Fix tsc ignore
    
    * Update dashboard url
    
    * Updated Navbar UI
    
    * Add nested routes (#28)
    
    * Update delegation page

--- src/pages/Delegate/index.tsx
+++ src/pages/Delegate/index.tsx
@@ -1,18 +1,31 @@
+import { isEmpty } from "lodash";
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import { useParams } from "wouter";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import LoadingPlaceholder from "../../components/LoadingPlaceholder";
+import { useGovNfts } from "../../hooks/govnft";
 import DelegateNft from "./components/DelegateNft";
 
 export default function Delegate() {
-  // TODO: Import real govnft that needs deletagion
-  const govnft = true;
+  const params = useParams();
+  const { address } = useAccount();
+  const { data: nfts } = useGovNfts(address);
+  const [nft, setNft] = useState(null);
+
+  useEffect(() => {
+    /* @ts-ignore */
+    const byId = nfts?.find((gnft) => gnft.id.toString() === params.id);
+    setNft(byId);
+  }, [nfts, params]);
 
   return (
     <div className="container mx-auto px-4 lg:px-8">
       {/* @ts-ignore */}
       <Header />
 
-      {!govnft ? <LoadingPlaceholder message="Loading data..." /> : <DelegateNft />}
+      {isEmpty(nft) ? <LoadingPlaceholder message="Loading data..." /> : <DelegateNft nft={nft} />}
 
       <Footer />
     </div>
