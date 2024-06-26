622c1247a755b94abe1d31bfb3cf41ec2c0c17d3
Alexandru G. <alexandrugugurel@gmail.com>
2024-06-27 00:40:26 +0300

    Add Etherscan Link

--- src/components/GovnftHeader.tsx
+++ src/components/GovnftHeader.tsx
@@ -1,5 +1,5 @@
 import { Button } from "flowbite-react";
-import { MoreVertical as MoreVerticalIcon, Share2 as Share2Icon } from "lucide-react";
+import { Link as LinkIcon, MoreVertical as MoreVerticalIcon } from "lucide-react";
 import { useState } from "react";
 import { useAccount } from "wagmi";
 
@@ -37,10 +37,9 @@ export default function GovnftHeader({
             <div className="flex text-gray-700 dark:text-gray-300">
               <GovnftNavbar nft={nft} active={active} />
             </div>
-            {/* TODO: Add eterscan url */}
-            <a href="https://etherscan.io/" target="_blank" rel="noreferrer">
-              <div className="flex gap-3 items-center px-7 py-4 text-gray-600 dark:text-gray-400 underline hover:no-underline">
-                <Share2Icon size={12} />
+            <a href={`https://etherscan.io/address/${nft.address}`} target="_blank" rel="noreferrer">
+              <div className="flex gap-2 items-center px-7 py-4 text-gray-600 dark:text-gray-400 underline hover:no-underline">
+                <LinkIcon size={12} />
                 Etherscan
               </div>
             </a>
