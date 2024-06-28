92caf9773d468aa502ccd4878ed5974d5231599c
Alexandru Gugurel <alexandrugugurel@gmail.com>
2024-06-29 01:07:39 +0300

    Add transfer redirect (#62)

--- src/pages/Transfer/components/TransferButton.tsx
+++ src/pages/Transfer/components/TransferButton.tsx
@@ -1,17 +1,31 @@
 import { Button } from "flowbite-react";
+import { useEffect } from "react";
 import { Address } from "viem";
 import { useWaitForTransactionReceipt, useWriteContract } from "wagmi";
 import { useAccount } from "wagmi";
+import { useLocation } from "wouter";
+
+import Toaster from "../../../components/Toaster";
 import { GOVNFT_ABI, GOVNFT_ADDRESS } from "../../../constants";
 
 export default function TransferButton({ id, recipient }: { id: bigint; recipient: Address }) {
   const { address } = useAccount();
-
+  const [, navigate] = useLocation();
   const { data: hash, error, isPending, writeContract } = useWriteContract();
 
   const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
     hash,
   });
+
+  useEffect(() => {
+    if (error) {
+      // @ts-ignore
+      Toaster.toast(error);
+    } else if (isConfirmed) {
+      navigate("~/dash");
+      Toaster.toast.success("GovNFT transfered!");
+    }
+  }, [error, isConfirmed, navigate]);
 
   return (
     <>
@@ -25,7 +39,7 @@ export default function TransferButton({ id, recipient }: { id: bigint; recipien
           })
         }
         className="w-full"
-        disabled={isPending || isConfirmed}
+        disabled={isPending || isConfirmed || isConfirming}
       >
         {isPending ? "Confirming..." : isConfirmed ? "Transferred" : "Transfer"}
       </Button>
