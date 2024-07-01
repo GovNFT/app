73e278ecba12e5a533f33c66de4cb67591d7b432
Stas SUȘCOV <stas@nerd.ro>
2024-07-01 18:06:12 +0100

    Routing: refactor to allow anon pageviews.

--- src/pages/Transfer/index.tsx
+++ src/pages/Transfer/index.tsx
@@ -2,11 +2,14 @@ import { isEmpty } from "lodash";
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { useParams } from "wouter";
+
 import Footer from "#/components/Footer";
 import Header from "#/components/Header";
 import LoadingPlaceholder from "#/components/LoadingPlaceholder";
+import { ZERO_ADDRESS } from "#/constants";
 import { useCollection } from "#/hooks/collection";
 import { useNft } from "#/hooks/govnft";
+
 import TransferNft from "./components/TransferNft";
 
 export default function Delegate() {
@@ -14,7 +17,7 @@ export default function Delegate() {
   const { id } = useParams();
   const { address } = useAccount();
   const collection = useCollection();
-  const { data: nft } = useNft(Number(id), address, collection?.address);
+  const { data: nft } = useNft(Number(id), address || ZERO_ADDRESS, collection?.address);
 
   return (
     <div className="container mx-auto px-4 lg:px-8">
