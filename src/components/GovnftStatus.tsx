321044a5b600aae9e2c10558dc8923fa18d56a0f
Alexandru G. <alexandrugugurel@gmail.com>
2024-07-02 16:49:46 +0300

    Update GovNFT Status

--- src/components/GovnftStatus.tsx
+++ src/components/GovnftStatus.tsx
@@ -11,33 +11,45 @@ export default function GovnftStatus({
     return <></>;
   }
 
-  return (
-    <div className="flex flex-col justify-center items-end rounded-md">
-      <div className="flex gap-3 items-center">
-        {!nft.vestingStarted && (
-          <div className="flex gap-2.5 items-center">
-            <div className="w-1.5 h-1.5 bg-amber-600 rounded-full animate-pulse" />
-            <DateFormat ts={nft.start} />
-          </div>
-        )}
-        {nft.vestingStarted && (
-          <div className="flex gap-2.5 items-center">
-            <div className="w-1.5 h-1.5 bg-green-500 rounded-full" />
-            <DateFormat ts={nft.end} />
-          </div>
-        )}
+  if (!nft.vestingStarted) {
+    return (
+      <div className="flex flex-col justify-center items-end rounded-md space-y-1.5">
+        <div className="flex gap-2.5 items-center">
+          <div className="w-1.5 h-1.5 bg-amber-600 rounded-full animate-pulse" />
+          <div className="text-amber-600">Scheduled</div>
+        </div>
+        <div className="text-xs text-gray-600 dark:text-gray-400">
+          <DateFromNow ts={nft.start} prefix="vesting starts in" pastPrefix="vesting started" tooltip={false} />
+        </div>
       </div>
+    );
+  }
 
-      {!nft.vestingStarted && (
-        <div className="text-amber-600 pt-1.5 text-xs">
-          <DateFromNow ts={nft.start} prefix="vesting starts in" tooltip={false} />
+  if (nft.vestingStarted && nft.vestedPct !== 100) {
+    return (
+      <div className="flex flex-col justify-center items-end rounded-md space-y-1.5">
+        <div className="flex gap-2.5 items-center">
+          <div className="w-1.5 h-1.5 bg-green-500 rounded-full animate-pulse" />
+          <div className="text-green-500">Vesting</div>
         </div>
-      )}
-      {nft.vestingStarted && (
-        <div className="text-gray-600 dark:text-gray-400 pt-1.5 text-xs">
-          <DateFromNow ts={nft.end} prefix="vesting ends in" pastPrefix="vesting ended" tooltip={false} />
+        <div className="text-xs text-gray-600 dark:text-gray-400">
+          <DateFromNow ts={nft.end} prefix="vesting ends" pastPrefix="vesting ended" tooltip={false} />
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
+
+  if (nft.vestingStarted && nft.vestedPct === 100) {
+    return (
+      <div className="flex flex-col justify-center items-end rounded-md space-y-1.5">
+        <div className="flex gap-2.5 items-center">
+          <div className="w-1.5 h-1.5 bg-gray-500 rounded-full animate-pulse" />
+          <div className="text-gray-500">Vested</div>
+        </div>
+        <div className="text-xs text-gray-600 dark:text-gray-400">
+          <DateFromNow ts={nft.end} prefix="vesting ends" pastPrefix="vesting ended" tooltip={false} />
+        </div>
+      </div>
+    );
+  }
 }
--- src/pages/Dashboard/components/Govnft.tsx
+++ src/pages/Dashboard/components/Govnft.tsx
@@ -25,24 +25,27 @@ export default function Govnft({
             <GovnftStatus nft={nft} />
             <GovnftProgress vestedPct={nft.vestedPct} vestingStarted={nft.vestingStarted} />
           </div>
-          <div className="px-6 py-2 w-52 hidden xl:flex flex-col gap-2 justify-center items-end border-l border-gray-100 dark:border-gray-950/20">
+          <div className="px-6 py-2 w-52 hidden xl:flex flex-col gap-1.5 justify-center items-end border-l border-gray-100 dark:border-gray-950/20">
             <div className="text-gray-400 dark:text-gray-600 text-xs">Locked</div>
             <Amount tokenAddress={nft.token} amount={nft.amount} showLogo={true} />
-            <div className="text-gray-600 dark:text-gray-400 text-xs flex gap-1 items-center">
-              of <Amount tokenAddress={nft.token} amount={nft.total_locked} showLogo={false} /> total
+            <div className="text-gray-400 dark:text-gray-600 text-xs flex gap-1 items-center">
+              claimed <Amount tokenAddress={nft.token} amount={nft.total_claimed} showLogo={false} />
             </div>
           </div>
-          <div className="sm:px-6 w-full sm:w-52 flex flex-row gap-6 sm:flex-col sm:gap-2 sm:justify-center items-center sm:items-end sm:border-l border-gray-100 dark:border-gray-950/20">
+          <div className="sm:px-3 w-full sm:w-52 flex flex-row gap-6 sm:flex-col sm:gap-1.5 sm:justify-center items-center sm:items-end sm:border-l border-gray-100 dark:border-gray-950/20">
             <div className="text-gray-400 dark:text-gray-600 text-xs">Claimable</div>
             <Amount tokenAddress={nft.token} amount={nft.claimable} showLogo={false} />
-            <div className="text-xs">
+            <div className="text-xs ">
               <ClaimButton nft={nft} />
+              {nft.claimable === 0n && (
+                <NavLink
+                  href={`/nft/${nft.id}`}
+                  className="text-gray-400 dark:text-gray-600 underline hover:no-underline"
+                >
+                  Review
+                </NavLink>
+              )}
             </div>
-            {nft.claimable === 0n && (
-              <NavLink href={`/nft/${nft.id}`}>
-                <div className="text-xs text-gray-400 dark:text-gray-600 underline hover:no-underline">Review</div>
-              </NavLink>
-            )}
           </div>
           <NavLink href={`/nft/${nft.id}`} className="flex h-full ">
             <div className="hidden sm:flex items-center px-3 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700/10 hover:dark:bg-gray-700/20 rounded-md cursor-pointer">
