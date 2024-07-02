e6e6a241f9bdf841a8f1d3154f5fdacb91bd8114
Alexandru Gugurel <alexandrugugurel@gmail.com>
2024-07-02 22:39:31 +0300

    Update GovNFT Status (#74)
    * Update GovNFT Status
    
    * rebased and ran linter
    
    ---------
    
    Co-authored-by: Isaac Ibiapina <mail@eyezick.com>

--- src/components/GovnftAvatar.tsx
+++ src/components/GovnftAvatar.tsx
@@ -4,7 +4,7 @@ import { SvgLoader } from "react-svgmt";
 
 import type { GovNft } from "#/hooks/types";
 import Amount from "./Amount";
-import DateFromNow from "./DateFromNow";
+import DateFormat from "./DateFormat";
 import NavLink from "./NavLink";
 
 export default function GovnftAvatar({
@@ -27,15 +27,21 @@ export default function GovnftAvatar({
         <div className="bg-gray-100 dark:bg-gray-700/20 h-14 w-14 hidden sm:flex items-center justify-center rounded-2xl">
           <SvgLoader src="/govnft-gray.svg" className="w-6" alt="GovNFT" />
         </div>
-        <div>
+        <div className="space-y-1">
           <div className="flex gap-2 items-center text-gray-800 dark:text-gray-200 font-semibold text-sm">
             GovNFT #{String(nft.id)}
             <LockIcon size={12} />
           </div>
-          <div className="text-xs pt-1.5 text-gray-600 dark:text-gray-400 whitespace-nowrap flex items-center">
-            <Amount amount={nft.amount} tokenAddress={nft.token} showLogo={false} />
-            &nbsp;
-            <DateFromNow ts={nft.end} prefix="vesting for" pastPrefix="vested" />
+          <div className="flex gap-1 items-center">
+            <div className="text-xs text-gray-600 dark:text-gray-400">Initial Lock:</div>
+            <div className="text-xs text-gray-600 dark:text-gray-400">
+              <Amount amount={nft.total_locked} tokenAddress={nft.token} showLogo={false} />
+            </div>
+          </div>
+          <div className="flex gap-1 items-center">
+            <div className="text-xs text-gray-400 dark:text-gray-600 ">
+              <DateFormat ts={nft.start} /> : <DateFormat ts={nft.end} />
+            </div>
           </div>
         </div>
       </div>
